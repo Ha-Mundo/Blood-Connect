68bf68ef31a5e225c369a13426f0516059ee3bbc
Ha-Mundo <trauzzu@gmail.com>
2026-04-05 19:07:01 +0100

    refactor: move tooltips initialization + flash msg timeout logic

--- static/js/ui-utils.js
+++ static/js/ui-utils.js
@@ -1,3 +1,18 @@
+// Enable Bootstrap tooltips 
+
+// Initialize all tooltips on the page
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(el => new bootstrap.Tooltip(el))
+
+// Flash messages timeout
+setTimeout(function() {
+    let alerts = document.querySelectorAll('#flash-container .alert');
+    alerts.forEach(function(alert) {
+        let bsAlert = new bootstrap.Alert(alert);
+        bsAlert.close();
+    });
+}, 5000); // 5000ms = 5 sec
+
 /* Function to toggle status view to confirm view for find_blood & donate page */
 function toggleConfirm(show){
     document.getElementById('status-view').style.display =
