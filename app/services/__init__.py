2b914c9dca762989331d8e1158b5700d60dbdeba
Ha-Mundo <trauzzu@gmail.com>
2026-04-13 21:32:18 +0100

    feat: add BloodService and AdminService to services exports

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,4 +1,6 @@
 from .auth_service import AuthService
 from .email_service import EmailService
+from .admin_service import AdminService
+from .blood_service import BloodService
 
-__all__ = ["AuthService", "EmailService"]
+__all__ = ["AuthService", "EmailService", "AdminService", "BloodService"]
